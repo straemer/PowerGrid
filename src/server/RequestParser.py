e11fb4b412b0ac9a1846a9f309a8ba3628ddb65c
Nikolai Semenenko <nsemenen@uwaterloo.ca>
2013-07-12 22:48:31 -0400

    Added ClientRequestTypes

--- src/server/RequestParser.py
+++ src/server/RequestParser.py
@@ -15,5 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with PowerGrid.  If not, see <http://www.gnu.org/licenses/>.
 
-def parseRequest ( request ):
-	
+    	
