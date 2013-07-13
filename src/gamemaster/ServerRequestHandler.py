92dc1937f6bf63200fe34ed63b51402fed45e42c
Nikolai Semenenko <nsemenen@uwaterloo.ca>
2013-07-13 15:37:28 -0400

    Added ServerRequestHander

---
+++ src/gamemaster/ServerRequestHandler.py
@@ -0,0 +1,48 @@
+# ServerRequestHandler.py
+# This file is a part of PowerGrid
+# Copyright 2013 Stephen Kraemer, Nikolai Semenenko
+
+# PowerGrid is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# PowerGrid is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with PowerGrid.  If not, see <http://www.gnu.org/licenses/>.
+
+from src.gamemaster.player import Player
+
+def moneyRequest(player):
+    pass
+
+def graphRequest():
+    pass
+
+def resourcesAvailable():
+    pass
+
+def powerPlantsAvailable():
+    pass
+
+def getPowerPlantInfo(powerPlantId):
+    pass 
+
+def getNodeInfo(nodeId):
+    pass
+
+def getPlayerPowerPlants(player):
+    pass
+
+def getPlayersInOrder():
+    pass
+
+def getPlayerResources(player):
+    pass
+
+def getStage()
+    pass
