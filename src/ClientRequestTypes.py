bfae7a62439dddbe46b56d64b4abb4e7e654a2c9
Nikolai Semenenko <nsemenen@uwaterloo.ca>
2013-07-12 22:51:49 -0400

    Actually added ClientRequestTypes

---
+++ src/ClientRequestTypes.py
@@ -0,0 +1,29 @@
+# ClientRequestTypes.py
+# This file is a part of PowerGrid
+# Copyright 2013 Stephen Kraemer, Nikolai Semenenko
+
+# PowerGrid is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# PowerGrid is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with PowerGrid.  If not, see <http://www.gnu.org/licenses/>.
+
+class ClientRequestTypes:
+    PLAYER_FUNDS = 0
+    GRAPH = 1
+    AVAILABLE_RESOURCES = 2
+    AVAILAVLE_POWER_PLANT = 3
+    POWER_PLANT_PROPERTIES = 4
+    NODE_INFO = 5
+    ALL_POWER_PLANTS = 6
+    PLAYERS_IN_ORDER = 7
+    PLAYER_RESOURCES = 8
+    CURRENT_STAGE = 9
+    
