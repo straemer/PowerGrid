ecacd998770b4188d5ff1ae8fd7b6e27e05cd8b5
ds2wang <ds2wang@uwaterloo.ca>
2013-07-13 17:14:46 -0400

    improved comments, removed useless methods

--- src/gamemaster/player.py
+++ src/gamemaster/player.py
@@ -1,17 +1,17 @@
 
 class Player:
     
+    # max_resources=max capacity for each type of resource
+    # [coal, oil, garbage, uranium]
+    # cities = array of nodes
+    # cities_powered = cites powered this turn
     def __init__(self):
         self.money=0
-        #[coal, oil, garbage, uranium]
-        self.resources=[0, 0, 0, 0]
+        self.resources=[0, 0, 0, 0]      
         self.cities=[]
         self.power_plants=[]
         self.max_plants=3
-        
-        #max capacity
-        self.max_resources=[0, 0, 0, 0]  
-        
+        self.max_resources=[0, 0, 0, 0]         
         self.cities_powered=0
 
     #returns int
@@ -142,48 +142,3 @@ class Player:
         else:
             for r in res_req:
                 self.cities.append(r)          
-'''      
-    def bid():
-        valid=False
-        while valid==False:
-            plant = int(input('Enter plant no.'))
-            cur_bid=int(input('Enter bid'))
-            if cur_bid==-1:
-                return
-            result=get_bid(plant, )
-            if result==true:
-                valid=true
-            
-    def update_bid(self, outbid, plant, maxbid):
-        if outbid:
-            valid=False
-            while valid==False:
-                cur_bid=int(input('Enter bid'))
-                if cur_bid==-1:
-                    return
-                result=get_bid(plant,cur_bid)          
-                if result==true:
-                    valid=true            
-        else:
-            if power_plants.count>3
-            remove_powerPlant(self)
-            self.power_plants.append(getPlant(plant))
-            self.money=self.money-maxbid
-            
-    def remove_powerPlant(self):
-        valid=False
-        while valid==False:
-            plant_no=int(input('Enter power plant to remove (1-3)'))
-            if plant_no>0 and plant_no<4:
-                self.power_plants.remove(plant_no) 
-      
-
-
-class PowerPlant:
-    cost=0
-    cities_powered=0
-    resource_types=[]
-    resource_capacity=0
-    resources_req=0 '''
-            
-
