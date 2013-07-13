8a269b14efe0bcd02ad881647691be071dadb44d
ds2wang <ds2wang@uwaterloo.ca>
2013-07-13 11:57:34 -0400

    Added accessors/mutators

--- src/gamemaster/player.py
+++ src/gamemaster/player.py
@@ -5,6 +5,7 @@ class Player:
     power_plants=[]
     max_plants=3
     max_resources=[]
+    cities_powered=0
     
     def __init__(self):
         self.money=0
@@ -13,12 +14,71 @@ class Player:
         self.power_plants=[]
         self.max_plants=3
         self.max_resources=[]  
+        self.cities_powered=0
     
-    def power_cities():
-        quit_game=False
-        player 
-       
-
+    def f_b_get_money(self):
+        return self.money
+    
+    def get_resources(self):
+        return self.resources
+    
+    def get_cities(self):
+        return self.cities
+    
+    def get_PowerPlants(self):
+        return self.power_plants
+    
+    def get_max_resources(self):
+        return self.get_max_resources
+    
+    def remove_PowerPlant(self, i):
+        self.power_plants.remove(i)
+    
+    def remove_resources(self, i):
+        self.resources.remove(i)
+        
+    def add_cities(self, city):
+        self.cities.append(city)
+    
+    def addMoney( self, dollars ):
+        self.money += dollars
+    
+    def calc_cities_powered(self, plants):
+        tot_powered=0
+        for p in plants:
+            tot_powered+=p.cities_powered
+        self.cities_powered=tot_powered
+    
+    def get_cities_powered(self):
+        return cities_powered
+    
+    def get_MaxValPowerPlant(self):
+        sorted_plants=self.power_plants.sort(key=PowerPlant.cost)
+        if sorted_plants.count==0:
+            return -1
+        return sorted_plants[0]
+    
+    def buy_resources(self, res_req):
+        tot_cost=0
+        if tot_cost==-1:
+            return
+        if tot_cost-self.money<0:
+            return
+        else:
+            for r in res_req:
+                self.resources.append(r)
+            self.money = self.money-tot_cost
+    
+    def buy_cities(cities_req):
+        tot_cost=0
+        if tot_cost == -1:
+            return
+        if tot_cost-self.money < 0:
+            return
+        else:
+            for r in res_req:
+                self.cities.append(r)          
+ """  placeholders         
     def bid():
         valid=False
         while valid==False:
@@ -26,7 +86,7 @@ class Player:
             cur_bid=int(input('Enter bid'))
             if cur_bid==-1:
                 return
-            result=get_bid(plant,cur_bid)
+            result=get_bid(plant, )
             if result==true:
                 valid=true
             
@@ -51,28 +111,8 @@ class Player:
         while valid==False:
             plant_no=int(input('Enter power plant to remove (1-3)'))
             if plant_no>0 and plant_no<4:
-                self.power_plants.remove(plant_no)
-            
-    def get_resources(self, res_req):
-        tot_cost=0
-        if tot_cost==-1:
-            return
-        if tot_cost-self.money<0:
-            return
-        else:
-            for r in res_req:
-                self.resources.append(r)
-            self.money = self.money-tot_cost
-    
-    def buy_cities(cities_req):
-        tot_cost=0
-        if tot_cost == -1:
-            return
-        if tot_cost-self.money < 0:
-            return
-        else:
-            for r in res_req:
-                self.cities.append(r)        
+                self.power_plants.remove(plant_no) 
+      
 
 
 class PowerPlant:
@@ -80,5 +120,6 @@ class PowerPlant:
     cities_powered=0
     resource_types=[]
     resource_capacity=0
-    resources_req=0
+    resources_req=0 """
+            
 
